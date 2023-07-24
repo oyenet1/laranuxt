901bf5b92bc794f69c278406e2225bf9dbb6077f
oyenet1 <bowofadeoyerinde@gmail.com>
2023-07-24 01:57:56 +0100

    login system in nuxt with tailwind installation

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: "#34a7a3",
+    },
+  },
+  plugins: [],
+}
+
